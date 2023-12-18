b86165b569e93d6ff6d33e2cf358d7cb3bfc89e1
jsepucas <jsepucas@myuax.com>
2023-12-18 13:14:04 +0100

    6

--- 2. Cadenas/2_1_EjercicioA.cpp
+++ 2. Cadenas/2_1_EjercicioA.cpp
@@ -3,3 +3,14 @@
 #include <stdio.h>
 
 using namespace std;
+
+
+int main (){
+    char cadena1[] = "El coche de allí";
+    cout << "La cadena 1 es: " << cadena1 << endl;
+
+    char cadena2[] = "Es verde ";
+    cout << "La cadena 2 es: " << cadena2 << endl;
+
+
+}
