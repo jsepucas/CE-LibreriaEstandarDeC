ad573cfc700bb306cbd73c53a0a5cdbdb1da165c
jsepucas <jsepucas@myuax.com>
2023-12-18 13:07:47 +0100

    4

--- Funciones del lenguaje C/2_EjercicioB.cpp
+++ Funciones del lenguaje C/2_EjercicioB.cpp
@@ -3,3 +3,13 @@
 #include <time.h>
 
 using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int num_rand = rand();
+    cout << "El numero aleatorio es: " << num_rand << endl;
+
+    return 0;
+
+}
