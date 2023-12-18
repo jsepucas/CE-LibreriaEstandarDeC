62da471d69a6872f5df9d12b5930d82140c8ee3e
jsepucas <jsepucas@myuax.com>
2023-12-18 13:03:43 +0100

    2 : )

--- Funciones del lenguaje C/1_EjercicioA.cpp
+++ Funciones del lenguaje C/1_EjercicioA.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+
+using namespace std;
+
+int main() {
+        int num = 29;
+
+        int abs_num = abs(num);
+
+        cout << "El valor absoluto de " << num << " es " << abs_num << endl;
+
+        return 0;
+}
